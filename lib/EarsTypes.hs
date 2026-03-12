5deb75db957a6bacd0c169353c104e4e2d74e5fc
Rafael Oliveira <raoliveira@aligntech.com>
2026-03-12 15:50:42 +0100

    EarsTypes

---
+++ lib/EarsTypes.hs
@@ -0,0 +1,39 @@
+module EarsTypes where
+
+import Text.Pandoc.Definition (Block)
+
+data Entity = MakeEntity {
+  entityLabel :: String,
+  entityIsPlural :: Bool,
+  entityIsDefined :: Bool,
+  entityDescription :: Block,
+  entityOptionalFeatures :: [String],
+  entityBoundaryEvents :: [String],
+  entityStates :: [String],
+  entityOutcomes :: [String] } deriving Show
+
+data Constraint = MakeConstraint {
+  constraintEntity :: Entity,
+  constraintFeatures :: [String],
+  constraintStates :: [String],
+  constraintEvents :: [String] } deriving Show
+
+data HappyPath = MakeHappyPath {
+  happyPathConstraints :: [Constraint],
+  happyPathOutcomes :: [String] } deriving (Show)
+
+data UnhappyPath = MakeUnhappyPath {
+  unhappyPathTriggeringEvents :: [String],
+  unhappyPathMitigationStrategy :: HappyPath } deriving Show
+
+data Behavior = TypicalBehavior HappyPath | MitigationBehavior UnhappyPath deriving Show
+
+data Requirement = MakeRequirement {
+  requirementLabel :: String,
+  requirementEntity :: Entity,
+  requirementBehavior :: Behavior,
+  requirementRationale :: String } deriving Show
+
+data Specification = MakeSpecification {
+  specificationRequirements :: [Requirement] } deriving (Show)
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,39 @@
 module Main (main) where
 
+import EarsTypes
+
+import Data.Text (pack)
+import Text.Pandoc.Definition (Block(..), Inline(..))
+
+entity1 :: Entity
+entity1 = MakeEntity {
+  entityLabel = "SystemXWZ",
+  entityIsPlural = False,
+  entityIsDefined = False,
+  entityDescription = Para [ Str (pack "The first entity.") ],
+  entityOptionalFeatures = [],
+  entityBoundaryEvents = [],
+  entityStates = [],
+  entityOutcomes = [] }
+
+happyPath1 :: HappyPath
+happyPath1 = MakeHappyPath {
+  happyPathConstraints = [],
+  happyPathOutcomes = [ "works" ] }
+
+requirement1 :: Requirement
+requirement1 = MakeRequirement {
+  requirementLabel = "REQ-01",
+  requirementEntity = entity1,
+  requirementBehavior = TypicalBehavior happyPath1,
+  requirementRationale = "The system must attend to expectations."
+}
+
+srs :: Specification
+srs = MakeSpecification {
+  specificationRequirements = [requirement1] }
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  print srs
+
