2b2a912212df8dbc19a718a9046d7b791366f9ab
Rafael Oliveira <raoliveira@aligntech.com>
2026-03-16 19:00:44 +0100

    req label separator

--- lib/EarsDoc.hs
+++ lib/EarsDoc.hs
@@ -21,9 +21,9 @@ instance EarsDoc Requirement where
   toBlocks _ requirement = _statement <> _rationale where
     _statement = para (_label <> _separator <> _text)
     _label = strong (text (T.pack (requirementLabel requirement)))
-    _separator = text (T.pack " ")
-    _text = text (T.pack ((makeStatement True requirement) ++ "."))
-    _rationale = para (text (T.pack ("Rationale: " ++ requirementRationale requirement)))
+    _separator = text (T.pack " - ")
+    _text = emph (text (T.pack ((makeStatement True requirement) ++ ".")))
+    _rationale = para (text (T.pack ("(Rationale: " ++ requirementRationale requirement ++ ")")))
 
 instance EarsDoc RequirementGroup where
   toBlocks headerLevel (MakeRequirementGroup title reqs) = _header <> _body where
